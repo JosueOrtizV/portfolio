10066a78044fb14d7044f398a43a2d93166678d9
Josue <josue_noob@hotmail.com>
2024-10-23 11:40:37 -0600

    text changes on proyects

--- src/app/data/project-data.ts
+++ src/app/data/project-data.ts
@@ -5,13 +5,13 @@ export const PROJECTS: Project[] = [
     {
         name: {
             en: 'Text Encryption & Decryption Tool',
-            es: '"Herramienta de Encriptación y Desencriptación de Texto'
+            es: 'Herramienta de Encriptación y Desencriptación de Texto'
         },
         images: ['./assets/img/encriptador/1.png','./assets/img/encriptador/2.png','./assets/img/encriptador/3.png','./assets/img/encriptador/4.png','./assets/img/encriptador/5.png',],
         languages: ['HTML', 'CSS', 'JavaScript'],
         description: {
             en: 'This project is a text encryption and decryption tool, created as part of the G5 - ONE Object-Oriented Java training within the Oracle Next Education program. The application features a <strong>responsive web design</strong>, ensuring optimal usability across various devices.',
-            es: '"Este proyecto es una herramienta de encriptación y desencriptación de texto, creada como parte de la formación Java Orientado a Objetos G5 - ONE del programa Oracle Next Education. La aplicación cuenta con un <strong>diseño web responsivo</strong>, garantizando una usabilidad óptima en diversos dispositivos.'
+            es: 'Este proyecto es una herramienta de encriptación y desencriptación de texto, creada como parte de la formación Java Orientado a Objetos G5 - ONE del programa Oracle Next Education. La aplicación cuenta con un <strong>diseño web responsivo</strong>, garantizando una usabilidad óptima en diversos dispositivos.'
         },
         codeLink: 'https://github.com/JosueOrtizV/Challenge-Oracle-Decoder',
         demoLink: 'https://josueortizv.github.io/Challenge-Oracle-Decoder/'
@@ -39,7 +39,7 @@ export const PROJECTS: Project[] = [
         languages: ['HTML', 'CSS','TypeScript', 'Angular'],
         description: {
             en: 'This is a portfolio project developed with Angular as part of the Master in JavaScript course: Learn JS, jQuery, Angular, NodeJS on Udemy. This segment represents the frontend component of the project. <strong>Responsive Web Design</strong> ensures optimal performance across all devices.',
-            es: '"Este es un proyecto de portafolio desarrollado con Angular como parte del curso Maestro en JavaScript: Aprende JS, jQuery, Angular, NodeJS en Udemy. Este segmento representa el componente frontend del proyecto. <strong>Diseño Web Responsivo</strong> garantiza un rendimiento óptimo en todos los dispositivos.'
+            es: 'Este es un proyecto de portafolio desarrollado con Angular como parte del curso Master en JavaScript: Aprende JS, jQuery, Angular, NodeJS en Udemy. Este segmento representa el componente frontend del proyecto. <strong>Diseño Web Responsivo</strong> garantiza un rendimiento óptimo en todos los dispositivos.'
         },
         codeLink: 'https://github.com/JosueOrtizV/AngularCourseProject/tree/main/frontend',
         demoLink: 'https://josueortizv.github.io/AngularCourseProject/'
@@ -47,13 +47,13 @@ export const PROJECTS: Project[] = [
     {
         name: {
             en: 'Master in JavaScript Course Project - CRUD API Backend',
-            es: 'Proyecto del Curso Maestro en JavaScript - Backend API CRUD'
+            es: 'Proyecto del Curso Master en JavaScript - Backend API CRUD'
         },
         images: ['./assets/img/proyecto-angular/3.png','./assets/img/proyecto-angular/1.png', './assets/img/proyecto-angular/2.png','./assets/img/proyecto-angular/4.png', './assets/img/proyecto-angular/5.png'],
         languages: ['Node.js', 'Javascript', 'MongoDB'],
         description: {
             en: 'This backend component of the Portfolio Project was developed during the Master in JavaScript course on Udemy. It includes a CRUD API that allows the frontend to create, read, update, and delete projects. Leveraging <strong>MongoDB</strong> as the database, this API facilitates seamless communication between the frontend and backend. Project images are uploaded to Imgbb via its API, with links stored in MongoDB, ensuring dynamic image retrieval for each project.',
-            es: '"Este componente backend del Proyecto de Portafolio fue desarrollado durante el curso Maestro en JavaScript en Udemy. Incluye una API CRUD que permite al frontend crear, leer, actualizar y eliminar proyectos. Utilizando <strong>MongoDB</strong> como base de datos, esta API facilita una comunicación fluida entre el frontend y el backend. Las imágenes de los proyectos se suben a Imgbb mediante su API, guardando los enlaces en MongoDB, garantizando la recuperación dinámica de imágenes para cada proyecto.'
+            es: '"Este componente backend del Proyecto de Portafolio fue desarrollado durante el curso Master en JavaScript en Udemy. Incluye una API CRUD que permite al frontend crear, leer, actualizar y eliminar proyectos. Utilizando <strong>MongoDB</strong> como base de datos, esta API facilita una comunicación fluida entre el frontend y el backend. Las imágenes de los proyectos se suben a Imgbb mediante su API, guardando los enlaces en MongoDB, garantizando la recuperación dinámica de imágenes para cada proyecto.'
         },
         codeLink: 'https://github.com/JosueOrtizV/AngularCourseProject/tree/main/backend',
         demoLink: 'https://josueortizv.github.io/AngularCourseProject/proyectos'
